66749d5e44b7ea73b11eb155eaf34598ee2f374a
Sergio Espinosa <sergioespinosa@Sergios-MacBook-Pro.local>
2021-08-01 19:10:07 -0500

    Password checker: return response api

--- checkmypass.py
+++ checkmypass.py
@@ -10,12 +10,21 @@ def request_api_data(query_char):
 		raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
 	return res
 
+def read_res(response):
+	print(response.text)
+
 def pwned_api_check(password):
 	#check password if it exist in API reponse
 	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-	return sha1password
+	first5_char, tail = sha1password[:5], sha1password[5:]
+	response = request_api_data(first5_char)
+	print(first5_char, tail)
+	print(response)
+	return read_res(response)
 
 pwned_api_check('123')
+
+
 
 
 
